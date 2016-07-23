5e8c3004779d78ec27891b3d63231cb9e71a42db
Eli Joesph Bradley <elijos03@gmail.com>
2016-07-23 12:21:04 -0500

    Initial Commit

---
+++ DotNetApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i=0; i<10; i++)
+        Console.Out.WriteLine("Hello "+Console.Out.Encoding);
+    }
+  }
+}
+
+
